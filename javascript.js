ccd0c4077a3a338dab9fefc3684b2105a5fd3c38
ItsATarp <weilu345@gmail.com>
2022-04-27 20:19:51 -0700

    added reset function

--- javascript.js
+++ javascript.js
@@ -12,14 +12,14 @@ const gamePlay = (() => {
     let tile = '';
     let winner = '';
     const place = (e,i) => {
-        console.log(e, i);
+        console.log(i);
     }
-    const setup = () => {
+    function setup() {
         const cells = document.querySelectorAll('.cell');
         for (let i = 0; i < cells.length; i++) {
             cells[i].addEventListener('click', place(cells[i],i));
         }
-    };
+    }
     const check = () => {
 
     }
@@ -27,6 +27,24 @@ const gamePlay = (() => {
     return {setup};
 }) ();
 
+const display = (() => {
+    const {board} = gameBoard;
+    const {setup} = gamePlay;
+    let cells = document.querySelectorAll('.cells');
+    function update() {
+        for(let i = 0; i < cells.length; i++){
+            cells[i].textContent = board[i];
+        }
+    }
+    const resetBtn = document.querySelector(".reset");
+    resetBtn.addEventListener('click', () =>{
+        for(let i=0; i < board.length; i++){
+            board[i]= '';
+        }
+        setup();
+        update();
+    });
+}) ();
 
 const p1 = Player("Player1", "O");
 const p2 = Player("Player2", "X");
